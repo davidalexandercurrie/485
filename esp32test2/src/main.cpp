8acfb04466ac95b4709fe0745c56f4e3875b2f6c
David Currie <david.alexander.currie@gmail.com>
2019-05-07 22:44:45 +1200

    lots of failed tests and some not failed tests

--- esp32test2/src/main.cpp
+++ esp32test2/src/main.cpp
@@ -1,9 +1,12 @@
 #include <Arduino.h>
 
-void setup() {
-  // put your setup code here, to run once:
+void setup()
+{
+  Serial.begin(115200);
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
+void loop()
+{
+  Serial.printf("hello");
+  delay(2000);
 }
